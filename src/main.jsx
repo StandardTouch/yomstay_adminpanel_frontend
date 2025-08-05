f03151426c260efcb568ab48896a03db6da6b9ad
yaseeen96 <syedyaseen1539@gmail.com>
2025-08-05 13:34:09 +0530

    update package.json

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { ProSidebarProvider } from "react-pro-sidebar";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { ThemeProvider } from "@/components/theme-provider";
 
@@ -11,9 +10,9 @@ const key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <ThemeProvider defaultTheme="system" enableSystem>
-    <ClerkProvider publishableKey={key}>
-      <App />
-    </ClerkProvider>
+      <ClerkProvider publishableKey={key}>
+        <App />
+      </ClerkProvider>
     </ThemeProvider>
   </StrictMode>
 );
